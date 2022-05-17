140ef240b9e54e10f3e4877cd30586174b8f2067
ElfGod <albertogx@icloud.com>
2022-05-17 12:08:26 -0500

    Passing Callback Functions Around

---
+++ 01-basics/test/lib/answer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  // _answerQuestion func pointer
+  // it's stored in a variable func selectHandler
+  final VoidCallback selectHandler;
+
+  Answer(this.selectHandler);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+          color: Colors.blue,
+          child: Text('Answer 1'),
+          onPressed: selectHandler),
+    );
+  }
+}
--- 01-basics/test/lib/main.dart
+++ 01-basics/test/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -38,18 +39,13 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
+          Question(questions[_questionIndex]),
             // ElevatedButton() is the new RaisedButton
-            RaisedButton(child: Text('Answer 1'), onPressed: _answerQuestion),
-            RaisedButton(
-                child: Text('Answer 2'),
-                onPressed: () => print('Answer 2 chosen!')),
-            RaisedButton(
-                child: Text('Answer 3'),
-                onPressed: () {
-                  // ... do some stuff
-                  print('Answer 3 chosen!');
-                }),
+            // Passing a func _answerQuestion
+            // to a Widget Answer
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
+            Answer(_answerQuestion),
           ],
         ),
       ),
