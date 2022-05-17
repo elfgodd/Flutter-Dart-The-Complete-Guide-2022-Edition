6e7d2f9ce96a9bd3a25048e3c7c7de8115325661
ElfGod <albertogx@icloud.com>
2022-05-17 10:35:37 -0500

    Using Private Properties

--- 01-basics/test/lib/main.dart
+++ 01-basics/test/lib/main.dart
@@ -8,19 +8,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
-  //_MyAppState createState() => _MyAppState();
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -36,9 +35,9 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Text(questions[_questionIndex]),
             // ElevatedButton() is the new RaisedButton
-            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 1'), onPressed: _answerQuestion),
             RaisedButton(
                 child: Text('Answer 2'),
                 onPressed: () => print('Answer 2 chosen!')),
