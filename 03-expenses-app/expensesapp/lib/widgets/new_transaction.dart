e2b9343e3f5739ca5db5461e26e1395e432e9014
ElfGod <albertogx@icloud.com>
2022-05-20 09:52:03 -0500

    Adding User Transactions to the List

--- 03-expenses-app/expensesapp/lib/widgets/new_transaction.dart
+++ 03-expenses-app/expensesapp/lib/widgets/new_transaction.dart
@@ -2,8 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
+  final Function addTx;
   final titleController = TextEditingController();
-  final amountController = TextEditingController(); 
+  final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +37,15 @@ class NewTransaction extends StatelessWidget {
               child: Text('Add Transaction'),
               textColor: Colors.purple,
               onPressed: (() {
+                // RF title is String but amount is a double
+                // We need logic here so users won't type Strings like Hello in the amount
+                addTx(
+                  titleController.text,
+                  double.parse(amountController.text));
                 // print(titleInput);
                 // print(amountInput);
-                print(titleController.text);
-                print(amountController.text);
+                // print(titleController.text);
+                // print(amountController.text);
               }),
             ),
           ],
