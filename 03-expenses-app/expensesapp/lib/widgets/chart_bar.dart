ace2151e85a376a0b9f750fab4aeab89db383e49
ElfGod <albertogx@icloud.com>
2022-05-23 16:23:55 -0500

    Improving the Overall Chart

--- 03-expenses-app/expensesapp/lib/widgets/chart_bar.dart
+++ 03-expenses-app/expensesapp/lib/widgets/chart_bar.dart
@@ -16,8 +16,11 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
+        Container(
+          height: 20,
+          child: FittedBox(
+              child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
+        ),
         SizedBox(height: 4),
         Container(
           height: 60,
