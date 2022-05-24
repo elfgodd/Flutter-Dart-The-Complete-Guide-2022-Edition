a98a0a62cec24933ead99afbc7ee2bd75385597c
ElfGod <albertogx@icloud.com>
2022-05-24 14:11:45 -0500

    Showing a DatePicker

--- 03-expenses-app/expensesapp/lib/widgets/new_transaction.dart
+++ 03-expenses-app/expensesapp/lib/widgets/new_transaction.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
 class NewTransaction extends StatefulWidget {
   final Function addTx;
 
@@ -10,13 +11,14 @@ class NewTransaction extends StatefulWidget {
 }
 
 class _NewTransactionState extends State<NewTransaction> {
-  final titleController = TextEditingController();
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
 
-  final amountController = TextEditingController();
 
-  void submitData() {
-    final enteredTitle = titleController.text;
-    final enteredAmount = double.parse(amountController.text);
+  void _submitData() {
+    final enteredTitle = _titleController.text;
+    final enteredAmount = double.parse(_amountController.text);
 
     if (enteredTitle.isEmpty || enteredAmount <= 0) {
       // This will stop the function execution
@@ -27,6 +29,27 @@ class _NewTransactionState extends State<NewTransaction> {
     widget.addTx(enteredTitle, enteredAmount);
     // This closes the modal after entering the data + enter
     Navigator.of(context).pop();
+  }
+
+  void _presentDatePicker() {
+    // This is a flutter func native
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2022),
+      lastDate: DateTime.now(),
+      // This is a Future
+      // The func will not pause for this to run
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      
+      setState(() {
+        _selectedDate = pickedDate;
+      });
+
+    });
   }
 
   @override
@@ -42,35 +65,39 @@ class _NewTransactionState extends State<NewTransaction> {
               decoration: InputDecoration(
                 labelText: 'Title',
               ),
-              controller: titleController,
-              onSubmitted: (_) => submitData(),
+              controller: _titleController,
+              onSubmitted: (_) => _submitData(),
               // onChanged: ((value) {
               //   titleInput = value;
               // }),
             ),
             TextField(
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Amount',
               ),
-              controller: amountController,
+              controller: _amountController,
               keyboardType: TextInputType.number,
-              onSubmitted: (_) => submitData(),
+              onSubmitted: (_) => _submitData(),
               // onChanged: (value) => amountInput = value,
             ),
             Container(
               height: 70,
               child: Row(
                 children: <Widget>[
-                  Text('No Date Chosen!'),
+                  Expanded(
+                    child: Text(_selectedDate == null
+                      ? 'No date chosen'
+                      : 'Picked Date: ${DateFormat.yMd().format(_selectedDate!)}',
+                      // : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
+                    ),
+                  ),
                   FlatButton(
                     textColor: Theme.of(context).primaryColor,
-                    child: Text(
-                      'Choose Date',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      )
-                    ),
-                    onPressed: () {},
+                    child: Text('Choose Date',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        )),
+                    onPressed: _presentDatePicker,
                   ),
                 ],
               ),
@@ -79,7 +106,7 @@ class _NewTransactionState extends State<NewTransaction> {
               child: Text('Add Transaction'),
               color: Theme.of(context).primaryColor,
               textColor: Theme.of(context).textTheme.button!.color,
-              onPressed: submitData,
+              onPressed: _submitData,
               // RF title is String but amount is a double
               // We need logic here so users won't type Strings like Hello in the amount
 
