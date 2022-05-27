a45ec266c83da5c601027e6b2ff77430f0c4583c
ElfGod <albertogx@icloud.com>
2022-05-27 18:25:53 -0500

    Using the LayoutBuilder Widget

--- 03-expenses-app/expensesapp/lib/widgets/chart_bar.dart
+++ 03-expenses-app/expensesapp/lib/widgets/chart_bar.dart
@@ -14,16 +14,17 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return LayoutBuilder(builder: (ctx, constraints) {
+      return Column(
       children: <Widget>[
         Container(
-          height: 20,
+          height: constraints.maxHeight * 0.15,
           child: FittedBox(
               child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
         ),
-        SizedBox(height: 4),
+        SizedBox(height: constraints.maxHeight * 0.05),
         Container(
-          height: 60,
+          height: constraints.maxHeight * 0.6,
           width: 10,
           child: Stack(
             children: <Widget>[
@@ -49,13 +50,15 @@ class ChartBar extends StatelessWidget {
             ],
           ),
         ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(
-          label,
+        SizedBox(height: constraints.maxHeight * 0.05),
+        Container(
+          height: constraints.maxHeight * 0.15,
+          child: FittedBox(
+            child: Text(label),
+          ),
         ),
       ],
-    );
+     );
+    },); 
   }
 }
