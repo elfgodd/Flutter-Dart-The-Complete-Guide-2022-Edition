d4aaf9b42375875626dcb5d2e0b2673bd908fab0
ElfGod <albertogx@icloud.com>
2022-05-27 19:10:20 -0500

    Rendering Alternative Landscape Content

--- 03-expenses-app/expensesapp/lib/main.dart
+++ 03-expenses-app/expensesapp/lib/main.dart
@@ -69,6 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
     //     date: DateTime.now(),
     //   ),
   ];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _userTransactions.where((tx) {
@@ -127,22 +128,38 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: appBar,
       body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              height: (MediaQuery.of(context).size.height -
-                      appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
-                  0.4,
-              child: Chart(_recentTransactions),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('Show Chart'),
+                Switch(
+                    value: _showChart,
+                    onChanged: (val) {
+                      setState(() {
+                        _showChart = val;
+                      });
+                    },
+                  ),
+              ],
             ),
-            Container(
+            _showChart
+            ?  Container(
                 height: (MediaQuery.of(context).size.height -
                         appBar.preferredSize.height -
                         MediaQuery.of(context).padding.top) *
-                    0.6,
-                child: TransactionList(_userTransactions, _deleteTransaction)),
+                    0.4,
+                child: Chart(_recentTransactions),
+              )
+            :  Container(
+                  height: (MediaQuery.of(context).size.height -
+                          appBar.preferredSize.height -
+                          MediaQuery.of(context).padding.top) *
+                      0.6,
+                  child: TransactionList(_userTransactions, _deleteTransaction)
+                ),
           ],
         ),
       ),
