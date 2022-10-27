6652cc94942367f4a04e23dea225f06b4b2ea2ea
ElfGod <albertogx@icloud.com>
2022-10-26 23:16:31 -0500

    Using the Device Size in Conditions

--- 03-expenses-app/expensesapp/lib/widgets/transaction_list.dart
+++ 03-expenses-app/expensesapp/lib/widgets/transaction_list.dart
@@ -57,11 +57,21 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(transactions[index].date),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => deleteTx(transactions[index].id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? TextButton.icon(
+                          style: TextButton.styleFrom(
+                              primary: Theme.of(context).errorColor),
+                          icon: Icon(Icons.delete),
+                          label: Text('Delete'),
+                          onPressed: () => deleteTx(
+                            transactions[index].id,
+                          ),
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transactions[index].id),
+                        ),
                 ),
               );
             },
