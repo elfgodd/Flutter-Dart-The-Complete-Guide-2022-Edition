9a28e6a53d0106dccfca494375d2fbdf2a1e59fd
ElfGod <albertogx@icloud.com>
2022-10-27 23:53:22 -0500

    Checking the Device Platform

--- 03-expenses-app/expensesapp/lib/main.dart
+++ 03-expenses-app/expensesapp/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -142,7 +144,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text('Show Chart'),
-                  Switch(
+                  Switch.adaptive(
+                    activeColor: Theme.of(context).accentColor,
                     value: _showChart,
                     onChanged: (val) => {
                       setState(() {
